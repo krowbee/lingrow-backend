db872eb0f016780c1721af9b881912008d0ed422
krowbee <skoresplay@gmail.com>
2025-12-31 16:55:38 +0200

    refactor: changed error if answer not found

--- src/domains/education/answer/answer.service.ts
+++ src/domains/education/answer/answer.service.ts
@@ -1,4 +1,4 @@
-import { BadRequestException, Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import PrismaService from 'src/lib/prisma/prisma.service';
 
 @Injectable()
@@ -14,7 +14,7 @@ export class AnswerService {
       select: { isCorrect: true },
     });
 
-    if (!answer) throw new BadRequestException('Incorrect answer data');
+    if (!answer) throw new NotFoundException('Incorrect answer data');
     return { isCorrect: answer.isCorrect };
   }
 }
