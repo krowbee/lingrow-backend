14f5ba7283f061b966a9414d05725effa503c71b
krowbee <skoresplay@gmail.com>
2026-02-15 22:20:00 +0200

    fix: connected task controller in module, fixed error with adminOnlyGuard

--- src/domains/auth/decorators/auth.decorators.ts
+++ src/domains/auth/decorators/auth.decorators.ts
@@ -2,7 +2,7 @@ import { UseGuards } from '@nestjs/common';
 import { AuthOnlyGuard } from '../guards/authOnly.guard';
 import { GuestOnlyGuard } from '../guards/guestOnly.guard';
 import { IncludeRefreshGuard } from '../guards/include-refresh.guard';
-import { AdminOnlyGuard } from '../guards/adminOnly.guard';
+import { AdminOnlyGuard } from '../guards/AdminOnly.guard';
 
 export const AuthOnly = () => UseGuards(AuthOnlyGuard);
 export const GuestOnly = () => UseGuards(GuestOnlyGuard);
